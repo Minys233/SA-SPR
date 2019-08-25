bed54feca92fd04255aa7b46cf32a2a980272ff1
Minys233 <minys@foxmail.com>
2019-08-26 01:24:33 +0800

    [upd] some test & try

--- get_data.py
+++ get_data.py
@@ -1,28 +1,56 @@
-import urllib.request
+import requests
 import hashlib
+from tqdm import tqdm
+import os
+from io import BytesIO
 
 
-def checksum(filename, md5):
+def checksum(filename):
     hash_md5 = hashlib.md5()
     with open(filename, 'rb') as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
-def download(url, filename, md5=None):
-    data = urllib.request.urlopen(url).read()
+def download(url, filename, md5=None, keep=False):
+    # check if the right file exists
+    if os.path.isfile(filename) and checksum(filename) == md5:
+        print(f"{filename} exists, MD5 hash check passed.")
+        return filename
+    # download it
+    response = requests.get(url, stream=True)
+    filesize = int(response.headers['content-length'])
+    hash_md5 = hashlib.md5()
     with open(filename, 'wb') as fout:
-        fout.write(data)
-    if not md5 is None:
-        local_md5 = checksum(filename, md5)
+        for chunk in tqdm(response.iter_content(4096), total=filesize//4096, ncols=80):
+            fout.write(chunk)
+            hash_md5.update(chunk)
+    local_md5 = hash_md5.hexdigest()
+    # check md5 hash of the file
+    if md5 is not None:
         if local_md5 == md5:
-            print(f"{filename}\tMD5 hash check passed")
+            print(f"{filename}\tMD5 hash check passed.")
+            return filename
         else:
-            print(f"{filename}\tMD5 hash check NOT passed, pls re-download")
+            if keep:
+                printf(f"{filename}\tMD5 hash check NOT passed, but kept.")
+                return filename
+            else:
+                print(f"{filename}\tMD5 hash check NOT passed, deleted.")
+                os.remove(filename)
+                return False
     else:
         print(f"{filename}\t not checking md5")
+        return filename
 
 if __name__ == '__main__':
+    #Mol2vec pre-trained model
+    download(
+        'https://github.com/samoturk/mol2vec/raw/master/examples/models/model_300dim.pkl',
+        'data/mol2vec_model_300dim.pkl',
+        '943260d383420e9ff19168dc59cdc99e'
+        )
+
     # ESOL data
     download(
         'https://cloud.tsinghua.edu.cn/f/2cc3b125053a4275b6a2/?dl=1',
