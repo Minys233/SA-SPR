74b2022e24ad318b149f10fd63411addfafd14e6
Minys <minys@foxmail.com>
2019-08-28 12:09:46 +0800

    [upd] update readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # SA-SPR
 Reproduction: Identifying Structure−Property Relationships through SMILES Syntax Analysis with Self-Attention Mechanism
+
+# Install
+The required packaged shall be installed by mixing pip and conda. Refer to `environment.yml` if your environmental requirements are not met.
+- conda
+    - rdkit
+    - gensim
+    - matplotlib
+    - numpy
+    - pandas
+    - scikit-learn
+    - scipy
+    - tqdm
+    - requests
+    
+- pip: 
+    - tensorflow / tensorflow-gpu
+    - mol2vec: via `pip install git+https://github.com/samoturk/mol2vec`
+
+Please first make a new dir `data` in order to run `get-data.py` to download training data and mol2vec pretrained model.
+
+Please then run `train-simple-bilstm.py` to train, grid search, evaluate the simple BiLSTM model. Feel free to modify the training script if you do not want to do grid search, refer to `solubility-simple-bilstm-summary.csv` to select hyper-parameters.
+
+Note: you may change `LSTM` to `CuDNNLSTM` in `model.py` in order to train with a GPU.
--- model.py
+++ model.py
@@ -1,5 +1,35 @@
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.python.keras import backend as K
+
+# refer to https://github.com/thushv89/attention_keras
+# as an example how to define a custom layer.
+class SelfAttentiveLayer(keras.layers.Layer):
+    def __init__(self, da, r, **kwargs):
+        super(SelfAttentiveLayer, self).__init__(**kwargs)
+        self.da = da
+        self.r = r
+
+    def build(self, input_shape):
+        # concat of outputs of BiLSTM, with shape (batch_size, n, vector_size)
+        assert isinstance(input_shape, tf.TensorShape)
+        n, vector_size = input_shape[1], input_shape[2]
+        self.W1 = self.add_weight(name='W1_attention',
+                                  shape=tf.TensorShape((self.da, vector_size)),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        self.W2 = self.add_weight(name='W2_attention',
+                                  shape=tf.TensorShape((self.r, self.da)),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+
+        # input : (batch_size, n, vector_size)
+        # 1. transpose to (batch_size, vector_size, n)
+        # 2. matmul with W1: (self.da, vector_size) * (batch_size, vector_size, n) = (batch_size, self.da, n)
+        # 3. tanh activation
+        # 4. matmul with W2: (self.r, self.da) * (batch_size, self.da, n) = (batch_size, self.r, n)
+        # 5. the output Ma will has a shape of (batch_size, self.r, n)
+        # NO BIAS
 
 
 def build_simple_bilstm_model(pad_to, vector_size, lstm_hidden):
@@ -13,6 +43,10 @@ def build_simple_bilstm_model(pad_to, vector_size, lstm_hidden):
     # y = keras.layers.Dense(1, activation='relu')(y)
     model = keras.models.Model(inputs=inputs, outputs=y)
     return model
+
+
+def build_sa_bilstm_model(pad_to, vector_size, lstm_hidden):
+    inputs = keras.layers.Input(shape=(pad_to, vector_size), dtype='float32')
 
 
 if __name__ == "__main__":
